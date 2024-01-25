ce92b3f869e093aa1e782b6cf2f3e4e8cb03542f
rajuljha <rajuljha49@gmail.com>
2024-01-26 01:31:30 +0530

    Add render to allowed hosts

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def customer_dashboard(request):
     return render(request, 'dashboard/customer_dashboard.html')
+
 @login_required
 def engineer_dashboard(request):
     return render(request, 'dashboard/engineer_dashboard.html')
